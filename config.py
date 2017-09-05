b0646f64485ba7e253b7398ca4c35ebb4f5ea45a
ahmadnourallah <ahmadnurallah@gmail.com>
2017-09-06 01:44:40 +0300

    make improvements and fix some problems

---
+++ config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+WEBSITENAME = "M.Hanny Sabbagh"
+WEBSITEDESC = "Free and open source software user & developer. This is my personal blog in English. All mentioned opinions are mine alone."
+DATABASE = os.path.join(os.getcwd(), "zayzafoun.db")
+DEBUG = True
+SECRET_KEY = os.urandom(20)
+USERNAME = "admin"
+PASSWORD = "7505d64a54e061b7acd54ccd58b49dc43500b635" # If you forget to modify this. You probably deserve it.
+DISQUSNAME = 'tajribython' # Don't forget to change the disqus name! Or the comments section won't be customized to yours, you should go to disqus.com and register a website there.
